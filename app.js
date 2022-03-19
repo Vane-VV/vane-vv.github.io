37744aa0a454c486146a6721af24852ca1521d8f
Vanessa Velasco-Valles <2023vvelasco@arisehighschool.org>
2022-03-19 15:05:55 -0700

    added a button and js for the button and css for the images card class

---
+++ app.js
@@ -0,0 +1,9 @@
+const thanks = document.getElementsByTagName('button')[0];
+console.log(thanks);
+
+thanks.addEventListener('click', () => {
+  thanks.textContent = 'Thank you for reading!';  
+                        })
+
+
+
